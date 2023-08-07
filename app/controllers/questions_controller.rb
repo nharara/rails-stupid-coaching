db06bb4a1f44fbd06b8d4f6f0a160e5a71cf6119
Nozomu Hara <slipper-spells-0j@icloud.com>
2023-08-07 13:00:58 +0900

    finished stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+# ;lfkdsa;lksdf;ljk
+
+# frozen_string_literal: true
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = if @question == 'I am going to work'
+                'Great!'
+              elsif @question.end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get back to work!"
+              end
+  end
+end
